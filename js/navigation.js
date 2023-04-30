1b95a87b3c99d0b4d0a4b7a0eafc08c047fcbf47
Leora Wenger <leora.wenger@mtahq.org>
2023-04-30 19:25:39 -0400

    blog mobile button toggle.

--- js/navigation.js
+++ js/navigation.js
@@ -3,25 +3,24 @@
  *
  * Handles toggling the navigation menu for small screens.
  */
-( function() {
-	window.onload = ()=>{
-	console.log('navigation foo')
-	const mobile = document.getElementById('leoraw-button');
-	const menu = document.getElementById( 'site-navigation' );
+(function () {
+	window.onload = () => {
+		const mobile = document.getElementById('leoraw-button');
+		const menu = document.getElementById('mobile-menu');
 
-	if(mobile){
+		if (mobile) {
 
-	mobile.onclick = function() {
-		console.log('mobile clicked')
-		if(menu.style.display === 'flex') {
-			menu.style.display = 'none';
+			mobile.onclick = function () {
+				console.log('mobile clicked')
+				if (menu.style.display === 'flex') {
+					menu.style.display = 'none';
+				}
+				else {
+					menu.style.display = 'flex';
+				}
+
+			};
 		}
-		else {
-			menu.style.display = 'flex';
-		}
-		
-	};
+
 	}
-
-}
-} )();
+})();
